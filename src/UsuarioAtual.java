89b03fb7960989f6b4136cc3196854833983d469
Ariel Riello <riello.programmer@gmail.com>
2023-04-30 03:48:00 -0300

    Lista de Animes v1.0

---
+++ src/UsuarioAtual.java
@@ -0,0 +1,15 @@
+package ListaDeAnimes.src;
+
+public class UsuarioAtual {
+    
+    private static String usuarioAtual = "";
+
+    public static String getUsuarioAtual() {
+        return usuarioAtual;
+    }
+
+    public static void setUsuarioAtual(String usuario) {
+        usuarioAtual = usuario;
+    }
+    
+}
